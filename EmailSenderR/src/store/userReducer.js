6e7b6fb06a16d06695808345c7cbca621d6dfa73
Tarun <tarun.khatana@janio.asia>
2022-12-13 20:13:29 +0530

    More changes to jodit editor code.

---
+++ EmailSenderR/src/store/userReducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userInfoSlice = createSlice({
+    name: "userInfo",
+    initialState: {
+        value: {},
+    },
+    reducers: {
+        setInfo: (state, action) => {
+            state.value = { ...state.value, ...action.payload };
+        },
+        removeInfo: (state) => {
+            state.value = {};
+        },
+    },
+});
+
+// Action creators are generated for each case reducer function
+export const { setInfo, removeInfo } = userInfoSlice.actions;
+
+export default userInfoSlice.reducer;
