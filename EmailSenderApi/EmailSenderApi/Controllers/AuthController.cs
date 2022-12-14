b871b9902874a0b4f3a7b6cbe1fa974abe35ce7c
Tarun <tarun.khatana@janio.asia>
2022-12-14 16:54:56 +0530

    1. AUTH code to delete users from SQLite Db 2. Verifying user from URL.

--- EmailSenderApi/EmailSenderApi/Controllers/AuthController.cs
+++ EmailSenderApi/EmailSenderApi/Controllers/AuthController.cs
@@ -1,18 +1,36 @@
 ﻿using System.Web.Http;
 using EmailSenderApi.DAL;
+using EmailSenderApi.Models.Response;
 
 namespace EmailSenderApi.Controllers
 {
     public class AuthController : BaseController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(int id)
         {
-            var userData = new UserService().GetUserById(loggedInUserId);
+            var userData = new UserService().GetUserById(id);
             return Ok(new
             {
                 allowAccess = userData != null ? userData.AllowAccess : false,
                 name = userData != null ? userData.Name : ""
             });
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(User model)
+        {
+            var dbService = new UserService();
+            var result = dbService.Create(model);
+            return Ok(result ? "OK" : "FAIL");
+        }
+
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var dbService = new UserService();
+            var result = dbService.Delete(id);
+            return Ok(result ? "OK" : "FAIL");
         }
     }
 }
--- EmailSenderApi/EmailSenderApi/DAL/UserService.cs
+++ EmailSenderApi/EmailSenderApi/DAL/UserService.cs
@@ -45,5 +45,57 @@ namespace EmailSenderApi.DAL
 
             return list.Count > 0 ? list[0] : default(User);
         }
+
+
+        /// <summary>
+        /// Save data in db
+        /// </summary>
+        /// <param name="User">User data</param>
+        /// <returns>Success/Failure</returns>
+        public bool Create(User data)
+        {
+            using (var connection = GetDbConnection())
+            {
+                string oString = $"INSERT INTO {Users}(Id, Name, AllowAccess) VALUES('{data.Id}', '{data.Name}', {(data.AllowAccess ? 1 : 0)});";
+                SQLiteCommand oCmd = new SQLiteCommand(oString, connection);
+                try
+                {
+                    connection.Open();
+                    oCmd.ExecuteNonQuery();
+                    connection.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Delete data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Delete(int id)
+        {
+            using (var connection = GetDbConnection())
+            {
+                string oString = $"DELETE FROM {Users} WHERE Id={id}";
+                var oCmd = new SQLiteCommand(oString, connection);
+                try
+                {
+                    connection.Open();
+                    var count = oCmd.ExecuteNonQuery();
+                    connection.Close();
+                    return count > 0;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
