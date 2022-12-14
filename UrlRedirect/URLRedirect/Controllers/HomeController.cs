9a321fd10b221e2505d9f03d3195e72884aea9dc
Tarun <tarun.khatana@janio.asia>
2022-12-14 13:37:31 +0530

    URL redirect code added.

---
+++ UrlRedirect/URLRedirect/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Web.Mvc;
+using System;
+
+namespace URLRedirect.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // GET Logged in user Id from IIS
+            var iisUserString = "AAAA\\1";
+            var loggedInUserIdArray = iisUserString.Split(new[] { "\\" }, StringSplitOptions.RemoveEmptyEntries);
+            if (loggedInUserIdArray == null && loggedInUserIdArray.Length < 2)
+                return View();
+
+            var userData = new
+            {
+                Id = loggedInUserIdArray[1],
+                Name = loggedInUserIdArray[0]
+            };
+
+            var allowedUsers = new List<string>();
+            var idsString = ConfigurationManager.AppSettings["AllowedAccess"];
+            if (!string.IsNullOrWhiteSpace(idsString))
+            {
+                allowedUsers = idsString.Split('|').ToList();
+            }
+
+            if (allowedUsers.IndexOf($"{userData.Id}") >= 0)
+                return Redirect($"{ConfigurationManager.AppSettings["ReactAppURL"]}?id={userData.Id}&name={userData.Name}");
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
