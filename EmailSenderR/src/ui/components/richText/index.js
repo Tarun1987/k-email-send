20d8877cde5f35bada0d753e773d7075ece0eb4a
Tarun <tarun.khatana@janio.asia>
2022-12-16 16:13:41 +0530

    Pasting of HTML allowed in jodit editor.

--- EmailSenderR/src/ui/components/richText/index.js
+++ EmailSenderR/src/ui/components/richText/index.js
@@ -3,70 +3,143 @@ import JoditEditor, { Jodit } from "jodit-react";
 import { useField } from "formik";
 import PropTypes from "prop-types";
 
-/**
- * @param {Jodit} jodit
- */
-function preparePaste(jodit) {
-    jodit.e.on(
-        "paste",
-        (e) => {
-            jodit.e.stopPropagation("paste");
-            jodit.s.insertHTML(Jodit.modules.Helpers.getDataTransfer(e).getData(Jodit.constants.TEXT_HTML).replace(/a/g, "b"));
-            return false;
+const copyStringToClipboard = function (str) {
+    var el = document.createElement("textarea");
+    el.value = str;
+    el.setAttribute("readonly", "");
+    el.style = { position: "absolute", left: "-9999px" };
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+};
+
+const facilityMergeFields = [
+    "FacilityNumber",
+    "FacilityName",
+    "Address",
+    "MapCategory",
+    "Latitude",
+    "Longitude",
+    "ReceivingPlant",
+    "TrunkLine",
+    "SiteElevation",
+];
+const inspectionMergeFields = ["InspectionCompleteDate", "InspectionEventType"];
+const createOptionGroupElement = (mergeFields, optionGrouplabel) => {
+    let optionGroupElement = document.createElement("optgroup");
+    optionGroupElement.setAttribute("label", optionGrouplabel);
+    for (let index = 0; index < mergeFields.length; index++) {
+        let optionElement = document.createElement("option");
+        optionElement.setAttribute("class", "merge-field-select-option");
+        optionElement.setAttribute("value", mergeFields[index]);
+        optionElement.text = mergeFields[index];
+        optionGroupElement.appendChild(optionElement);
+    }
+    return optionGroupElement;
+};
+const buttons = [
+    "undo",
+    "redo",
+    "|",
+    "bold",
+    "strikethrough",
+    "underline",
+    "italic",
+    "|",
+    "superscript",
+    "subscript",
+    "|",
+    "align",
+    "|",
+    "ul",
+    "ol",
+    "outdent",
+    "indent",
+    "|",
+    "font",
+    "fontsize",
+    "brush",
+    "paragraph",
+    "|",
+    "image",
+    "link",
+    "table",
+    "|",
+    "hr",
+    "eraser",
+    "copyformat",
+    "|",
+    "fullsize",
+    "selectall",
+    "print",
+    "|",
+    "source",
+    "|",
+    {
+        name: "insertMergeField",
+        tooltip: "Insert Merge Field",
+        iconURL: "images/merge.png",
+        popup: (editor, current, self, close) => {
+            function onSelected(e) {
+                let mergeField = e.target.value;
+                if (mergeField) {
+                    console.log(mergeField);
+                    editor.selection.insertNode(editor.create.inside.fromHTML("{{" + mergeField + "}}"));
+                }
+            }
+            let divElement = editor.create.div("merge-field-popup");
+
+            let labelElement = document.createElement("label");
+            labelElement.setAttribute("class", "merge-field-label");
+            labelElement.text = "Merge field: ";
+            divElement.appendChild(labelElement);
+
+            let selectElement = document.createElement("select");
+            selectElement.setAttribute("class", "merge-field-select");
+            selectElement.appendChild(createOptionGroupElement(facilityMergeFields, "Facility"));
+            selectElement.appendChild(createOptionGroupElement(inspectionMergeFields, "Inspection"));
+            selectElement.onchange = onSelected;
+            divElement.appendChild(selectElement);
+
+            console.log(divElement);
+            return divElement;
         },
-        { top: true }
-    );
-}
-Jodit.plugins.add("preparePaste", preparePaste);
+    },
+    {
+        name: "copyContent",
+        tooltip: "Copy HTML to Clipboard",
+        iconURL: "images/copy.png",
+        exec: function (editor) {
+            let html = editor.value;
+            copyStringToClipboard(html);
+        },
+    },
+];
+
+const editorConfig = {
+    readonly: false,
+    toolbar: true,
+    spellcheck: true,
+    language: "en",
+    toolbarButtonSize: "medium",
+    toolbarAdaptive: false,
+    showCharsCounter: true,
+    showWordsCounter: true,
+    showXPathInStatusbar: false,
+    askBeforePasteHTML: true,
+    askBeforePasteFromWord: true,
+    //defaultActionOnPaste: "insert_clear_html",
+    buttons: buttons,
+    uploader: {
+        insertImageAsBase64URI: true,
+    },
+    height: 400,
+};
 
 const RichText = (props) => {
     const editor = useRef(null);
-    // const config = {
-    //     readonly: false,
-    //     height: 400,
-    //     autofocus: true,
-    //     toolbarAdaptive: true,
-    //     buttons:
-    //         "bold,italic,underline,strikethrough,eraser,|,ul,ol,|,font,fontsize,paragraph,classSpan,lineHeight,|,superscript,subscript,image,\n,table,cut,copy,paste,selectall,copyformat,hr",
-    // };
-    const config = {
-        zIndex: 100,
-        colorPickerDefaultTab: "background",
-        height: 400,
-        buttons: [
-            "bold",
-            "strikethrough",
-            "underline",
-            "italic",
-            "|",
-            "ul",
-            "ol",
-            "|",
-            "outdent",
-            "indent",
-            "|",
-            "font",
-            "fontsize",
-            "brush",
-            "paragraph",
-            "\n",
-            "image",
-            "table",
-            "link",
-            "|",
-            "align",
-            "undo",
-            "redo",
-            "|",
-            "eraser",
-            "copyformat",
-            "|",
-            "fullsize",
-            "print",
-        ],
-        buttonsXS: ["bold", "image", "|", "brush", "paragraph", "|", "align", "|", "undo", "redo", "|", "eraser", "dots"],
-        textIcons: false,
-    };
+
     const [field, meta, helpers] = useField(props.name);
 
     const { value } = meta;
@@ -84,7 +157,7 @@ const RichText = (props) => {
             <label htmlFor={props.id}>{props.label}</label>
             <br />
             <div className="_RichText">
-                <JoditEditor ref={editor} value={value} config={config} onBlur={handleUpdate} onChange={handleChange} />
+                <JoditEditor ref={editor} value={value} config={editorConfig} onBlur={handleUpdate} onChange={handleChange} />
             </div>
 
             {meta.touched && meta.error && (
